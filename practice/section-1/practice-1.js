4814d1d722661db13059b24dae7a6a59787c8043
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-09 12:03:38 +0800

    Practice-1 Refactored

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,11 +1,5 @@
 'use strict';
 
-function collectSameElements(collectionA, collectionB) {  
-  const finalCollection = [];
-  collectionA.forEach((collection_a)=>collectionB.forEach((collection_b)=>{
-    if ( collection_a === collection_b ){
-      finalCollection.push(collection_a);
-    }
-  }));
-  return finalCollection;
+function collectSameElements(collectionA, collectionB) {
+  return collectionA.filter(listA=>collectionB.includes(listA));
 }
