6b6ba26dbae245c407c82b8525790093fedbe010
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-09 12:04:25 +0800

    Practice-3 refactored

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,11 +1,5 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  const finalCollection = [];
-  collectionA.forEach((collection_a)=>Object.values(objectB)[0].forEach((collection_b)=>{
-    if ( collection_a === collection_b ){
-      finalCollection.push(collection_a);
-    }
-    }));
-    return finalCollection;
+  return collectionA.filter(listA=>Object.values(objectB)[0].includes(listA));
 }
